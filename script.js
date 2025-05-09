d27822534a9eba01f09600f68a1391e0f6d23130
FraDepi01 <francesco-fdp@hotmail.it>
2025-05-09 22:39:58 +0000

    sistemazione generale

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
         burgerMenu.classList.toggle("active"); // Animate the burger menu
         menuLinks.classList.toggle("active"); // Show or hide the menu
     });
+
+    const dynamicText = document.querySelector('.dynamic-text');
+    const roles = ["Graphic Designer", "Illustratore", "Brand Designer", "Modellatore 3D", "Montatore video"];
+    let index = 0;
+
+    function changeText() {
+        dynamicText.textContent = roles[index];
+        index = (index + 1) % roles.length; // Loop through roles
+    }
+
+    setInterval(changeText, 1750); // Change text every 2 seconds
 });
